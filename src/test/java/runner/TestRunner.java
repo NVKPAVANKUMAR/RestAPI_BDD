ddfe106da0fd37ad440be30ac3796438de61e507
NVKPAVANKUMAR <npavankumar144@gmail.com>
2021-08-10 14:11:05 +0530

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/APIFeatures", glue = {"stepDefs"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+        , monochrome = true
+        //,publish = true
+)
+public class TestRunner {
+
+}
+
